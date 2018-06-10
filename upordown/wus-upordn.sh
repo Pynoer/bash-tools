64b0026f987514daf661dd00a932e2c593bb2c8a
wuseman <info@sendit.nu>
2018-06-10 12:18:54 +0200

    is host/ip up or down?

---
+++ upordown/wus-upordn.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+##################################################################################
+##### LICENSE ####################################################################
+##################################################################################
+####                                                                          ####
+#### Copyright (C) 2018 wuseman <info@sendit.nu>                              ####
+####                                                                          ####
+#### This program is free software: you can redistribute it and/or modify     ####
+#### it under the terms of the GNU General Public License as published by     ####
+#### the Free Software Foundation, either version 3 of the License, or        ####
+#### (at your option) any later version.                                      ####
+####                                                                          ####
+#### This program is distributed in the hope that it will be useful,          ####
+#### but WITHOUT ANY WARRANTY; without even the implied warranty of           ####
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ####
+#### GNU General Public License at <http://www.gnu.org/licenses/> for         ####
+#### more details.                                                            ####
+####                                                                          ####
+##################################################################################
+##### GREETINGS ##################################################################
+##################################################################################
+####                                                                          ####
+#### To all developers that contributes to all kind of open source projects   ####
+#### Keep up the good work!                                                   #<3#
+####                                                                          ####
+#### https://sendit.nu & https://github.com/wuseman                           ####
+####                                                                          ####
+##################################################################################
+#### Description #################################################################
+##################################################################################
+####                                                                          ####
+#### This script will tell if a site/host is up or down from linux cli        ####
+####                                                                          ####
+#### Author:  wuseman                                                         ####
+#### Contact: info@sendit.nu                                                  ####
+#### IRC:     wuseman @ Freenode.com                                          ####
+#### Distro:  This script has been tested on: Gentoo                          ####
+#### Usage:   bash wus-upordown.sh <ip/host>                                  ####
+####                                                                          ####
+#### Enjoy another 'bash' script from wuseman. Questions? Contact me!         ####
+####                                                                          ####
+##################################################################################
+#### Begin of code ###############################################################
+##################################################################################
+
+read -p "IP/Hostname: " iphost
+if ping -c 1 $iphost &> /dev/null
+then
+  echo -e 'Status: \e[32mUP'
+else
+  echo -e 'Status: \e[31mDown'
+fi
