b36ed2b0439776b3ac4cdf57ee3e6b27ef2cd9b5
wuseman <info@sendit.nu>
2018-06-10 12:17:07 +0200

    a minmial calculator

---
+++ calc/wus-c.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+##################################################################################
+##### LICENSE ####################################################################
+##################################################################################
+####                                                                          ####
+#### Copyright (C) 2018 wuseman <info@sendit.nu>                              ####
+####                                                                          ####
+#### This program is free software: you can redistribute it and/or modify     ####
+#### it under the terms of the GNU General Public License as published by     ####
+#### the Free Software Foundation, either version 3 of the License, or        ####
+#### (at your option) any later version.                                      ####
+####                                                                          ####
+#### This program is distributed in the hope that it will be useful,          ####
+#### but WITHOUT ANY WARRANTY; without even the implied warranty of           ####
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ####
+#### GNU General Public License at <http://www.gnu.org/licenses/> for         ####
+#### more details.                                                            ####
+####                                                                          ####
+##################################################################################
+##### GREETINGS ##################################################################
+##################################################################################
+####                                                                          ####
+#### To all developers that contributes to all kind of open source projects   ####
+#### Keep up the good work!                                                   #<3#
+####                                                                          ####
+#### https://sendit.nu & https://github.com/wuseman                           ####
+####                                                                          ####
+##################################################################################
+#### DESCRIPTION #################################################################
+##################################################################################
+####                                                                          ####
+#### wus-c.sh is a very simple but powerful calculator                        ####
+####                                                                          ####
+#### Author:  wuseman                                                         ####
+#### Contact: info@sendit.nu                                                  ####
+#### IRC:     wuseman @ Freenode.com                                          ####
+#### Distro:  This script has been tested on: Lenovo Thinkpad W541 with Gentoo####
+####                                                                          ####
+#### Usage:   bash wus-c.sh                                                   ####
+####                                                                          ####
+#### Enjoy another awesome 'bash' script from wuseman. Questions? Conact me!  ####
+####                                                                          ####
+##################################################################################
+#### Begin of code  ##############################################################
+
+echo $1 $2 | bc
