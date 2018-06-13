9fb9cc0672dc54352361f73abab360b9cbaa2940
wuseman <info@sendit.nu>
2018-06-13 19:05:32 +0200

    two new script has been added for create valid random MAC adresses

---
+++ mac-addr-creator/wuz-macaddrcreator.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+##################################################################################
+##### LICENSE ####################################################################
+##################################################################################
+####                                                                          ####
+#### Copyright (C) 2018 wuseman <info@sendit.nu>                              ####
+####                                                                          ####
+#### This program is free software: you can redistribute it and/or modify     ####
+#### it under the terms of the GNU General Public License as published by     ####
+#### the Free Software Foundation, either version 3 of the License, or        ####
+#### (at your option) any later version.                                      ####
+####                                                                          ####
+#### This program is distributed in the hope that it will be useful,          ####
+#### but WITHOUT ANY WARRANTY; without even the implied warranty of           ####
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ####
+#### GNU General Public License at <http://www.gnu.org/licenses/> for         ####
+#### more details.                                                            ####
+####                                                                          ####
+##################################################################################
+##### GREETINGS ##################################################################
+##################################################################################
+####                                                                          ####
+#### To all developers that contributes to all kind of open source projects   ####
+#### Keep up the good work!                                                   #<3#
+####                                                                          ####
+#### https://sendit.nu & https://github.com/wuseman                           ####
+####                                                                          ####
+##################################################################################
+#### DESCRIPTION #################################################################
+##################################################################################
+####                                                                          ####
+#### Generate a valid random MAC Address                                      ####
+####                                                                          ####
+#### Author:  wuseman                                                         ####
+#### Contact: info@sendit.nu                                                  ####
+#### IRC:     wuseman @ Freenode.com                                          ####
+#### Distro:  This script has been tested on: Gentoo                          ####
+####                                                                          ####
+#### Usage:   bash wus-macaddcreator.sh                                       ####
+####                                                                          ####
+#### Enjoy another awesome 'bash' script from wuseman. Questions? Conact me!  ####
+####                                                                          ####
+##################################################################################
+#### Begin of code  ##############################################################
+##################################################################################
+
+hexchars="0123456789ABCDEF"
+end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/-\1/g' )
+echo 00-60-2F$end
---
+++ mac-addr-creator/wuz-macaddrcreator2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+##################################################################################
+##### LICENSE ####################################################################
+##################################################################################
+####                                                                          ####
+#### Copyright (C) 2018 wuseman <info@sendit.nu>                              ####
+####                                                                          ####
+#### This program is free software: you can redistribute it and/or modify     ####
+#### it under the terms of the GNU General Public License as published by     ####
+#### the Free Software Foundation, either version 3 of the License, or        ####
+#### (at your option) any later version.                                      ####
+####                                                                          ####
+#### This program is distributed in the hope that it will be useful,          ####
+#### but WITHOUT ANY WARRANTY; without even the implied warranty of           ####
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ####
+#### GNU General Public License at <http://www.gnu.org/licenses/> for         ####
+#### more details.                                                            ####
+####                                                                          ####
+##################################################################################
+##### GREETINGS ##################################################################
+##################################################################################
+####                                                                          ####
+#### To all developers that contributes to all kind of open source projects   ####
+#### Keep up the good work!                                                   #<3#
+####                                                                          ####
+#### https://sendit.nu & https://github.com/wuseman                           ####
+####                                                                          ####
+##################################################################################
+#### DESCRIPTION #################################################################
+##################################################################################
+####                                                                          ####
+#### Generate a valid random MAC Address                                      ####
+####                                                                          ####
+#### Author:  wuseman                                                         ####
+#### Contact: info@sendit.nu                                                  ####
+#### IRC:     wuseman @ Freenode.com                                          ####
+#### Distro:  This script has been tested on: Gentoo                          ####
+####                                                                          ####
+#### Usage:   bash wus-macaddcreator.sh                                       ####
+####                                                                          ####
+#### Enjoy another awesome 'bash' script from wuseman. Questions? Conact me!  ####
+####                                                                          ####
+##################################################################################
+#### Begin of code  ##############################################################
+##################################################################################
+
+RANGE=255
+
+number=$RANDOM
+numbera=$RANDOM
+numberb=$RANDOM
+
+let "number %= $RANGE"
+let "numbera %= $RANGE"
+let "numberb %= $RANGE"
+
+octets='00-60-2F'
+
+octeta=`echo "obase=16;$number" | bc`
+octetb=`echo "obase=16;$numbera" | bc`
+octetc=`echo "obase=16;$numberb" | bc`
+macadd="${octets}-${octeta}-${octetb}-${octetc}"
+
+echo $macadd
