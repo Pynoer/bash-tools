68b498016aaa5c095a3469595473149f1c62bb8f
wuseman <info@sendit.nu>
2018-06-17 06:27:56 +0200

    compare 2 files splitted on screen

---
+++ diff/README.md
@@ -0,0 +1,3 @@
+# Preview
+
+![alt text](https://sendit.nu/f/o4Lhyi6sbrFe7b.png)
---
+++ diff/wus-diff.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+##################################################################################
+##### LICENSE ####################################################################
+##################################################################################
+####                                                                          ####
+#### Copyright (C) 2018 wuseman <info@sendit.nu>                              ####
+####                                                                          ####
+#### This program is free software: you can redistribute it and/or modify     ####
+#### it under the terms of the GNU General Public License as published by     ####
+#### the Free Software Foundation, either version 3 of the License, or        ####
+#### (at your option) any later version.                                      ####
+####                                                                          ####
+#### This program is distributed in the hope that it will be useful,          ####
+#### but WITHOUT ANY WARRANTY; without even the implied warranty of           ####
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ####
+#### GNU General Public License at <http://www.gnu.org/licenses/> for         ####
+#### more details.                                                            ####
+####                                                                          ####
+##################################################################################
+##### GREETINGS ##################################################################
+##################################################################################
+####                                                                          ####
+#### To all developers that contributes to all kind of open source projects   ####
+#### Keep up the good work!                                                   #<3#
+####                                                                          ####
+#### https://sendit.nu & https://github.com/wuseman                           ####
+####                                                                          ####
+##################################################################################
+#### DESCRIPTION #################################################################
+##################################################################################
+####                                                                          ####
+#### Compare two files splitted on screen                                     ####
+####                                                                          ####
+#### Author:  wuseman                                                         ####
+#### Contact: info@sendit.nu                                                  ####
+#### IRC:     wuseman @ Freenode.com                                          ####
+#### Distro:  This script has been tested on: Lenovo Thinkpad W541 with Gentoo####
+####                                                                          ####
+#### Usage:   bash wLuks.sh                                                   ####
+####                                                                          ####
+#### Enjoy another awesome 'bash' script from wuseman. Questions? Conact me!  ####
+####                                                                          ####
+##################################################################################
+#### Begin of code  ##############################################################
+
+
+cat <<EOF
+           ____  _ ________
+ _      __/ __ \(_) __/ __/
+| | /| / / / / / / /_/ /_  
+| |/ |/ / /_/ / / __/ __/  
+|__/|__/_____/_/_/ /_/     
+                           
+      Author: wuseman 
+
+EOF
+
+read -p "First File: " first
+read -p "Second File: " second
+echo -e "\nPrinting files:\n"
+diff -y -W 120 $first $second
