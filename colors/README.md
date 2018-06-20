2b663fcc01f6021923be6f958f9b82f2a5d261ed
wuseman <info@sendit.nu>
2018-06-20 22:22:09 +0200

    added another preview

--- colors/README.md
+++ colors/README.md
@@ -1,2 +1,3 @@
 # Preview
 ![alt text](https://sendit.nu/f/zGLihsEE%3Fru2H8.png)
+![alt text](https://sendit.nu/f/m96GLvblG8g9f-.png)
--- colors/wus-colors.sh
+++ colors/wus-colors.sh
@@ -172,3 +172,37 @@ echo -e "\e[46mHello this is a color test from mr.wuseman\e[0m"
 echo -e "\e[47mHello this is a color test from mr.wuseman\e[0m"
 
 
+
+echo -en "\n   +  "
+for i in {0..35}; do
+  printf "%2b " $i
+done
+
+printf "\n\n %3b  " 0
+for i in {0..15}; do
+  echo -en "\033[48;5;${i}m  \033[m "
+done
+
+#for i in 16 52 88 124 160 196 232; do
+for i in {0..6}; do
+  let "i = i*36 +16"
+  printf "\n\n %3b  " $i
+  for j in {0..35}; do
+    let "val = i+j"
+    echo -en "\033[48;5;${val}m  \033[m "
+  done
+done
+
+echo -e "\n"
+
+wuzi="wuseman"
+echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m";
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m';
+  do FG=${FGs// /}
+  echo -en " $FGs \033[$FG  $wuzi  "
+  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+    do echo -en "$EINS \033[$FG\033[$BG  $wuzi \033[0m\033[$BG \033[0m";
+  done
+  echo;
+done
+echo
